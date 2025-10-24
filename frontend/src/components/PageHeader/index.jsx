45a5c3d2baf1cbacdc46752eaaa7e5ab634f1ebb
Jean Cássio <jc.cassio1702@gmail.com>
2025-10-24 16:54:41 -0300

    feat(frontend): create PageHeader and LoadingScreen components

---
+++ frontend/src/components/PageHeader/index.jsx
@@ -0,0 +1,19 @@
+import styles from "./PageHeader.module.css";
+import { Link } from "react-router-dom";
+
+import PrimaryButton from "../PrimaryButton";
+
+const PageHeader = ({ title, buttonText, route }) => {
+  return (
+    <div className={styles.wrapper}>
+      <h2 className={styles.title}>{title}</h2>
+      {!!buttonText && (
+        <Link to={route}>
+          <PrimaryButton>{buttonText}</PrimaryButton>
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default PageHeader;
