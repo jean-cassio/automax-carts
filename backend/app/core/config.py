e5343bb3398c13e3b0bbe9e70dee01e73489b3f3
Jean Cássio <jc.cassio1702@gmail.com>
2025-10-24 10:30:26 -0300

    chore: add .gitignore and environment variables support

---
+++ backend/app/core/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Settings:
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    FAKE_STORE_API: str = os.getenv("FAKE_STORE_API")
+    SYNC_INTERVAL_HOURS: int = int(os.getenv("SYNC_INTERVAL_HOURS", 6))
+
+
+settings = Settings()
